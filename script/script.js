0f8ef67d386d0496c517cfd93210673996b01c52
whkim94 <whkim94@gmail.com>
2019-04-19 14:37:37 -0700

    Added skill bar animation using jQuery

--- script/script.js
+++ script/script.js
@@ -1,23 +1,34 @@
 // Prevents user from right click and drag
 
 $(document).ready(function() {
-  $(document).bind("contextmenu", function (e) {
+  $(document).bind("contextmenu", function () {
     return false;
   });
 
-  $('img').bind("contextmenu", function (e) {
+  $('img').bind("contextmenu", function () {
     return false;
   });
 
-  $('img').bind("selectstart", function (e) {
+  $('img').bind("selectstart", function () {
     return false;
   });
 });
 document.oncontextmenu = function () { return false; }
 document.onselectstart = function () { return false; }
 
-// Loads animation prior to the main contents
+// Loads animation triggered once website is loaded
 
 $(window).on('load',function() {
   $('.preloader').addClass('complete');
 });
+
+
+// Progress animations is triggered when viewer scrolls down into skills section
+
+$(document).on('scroll', function() {
+  // var scrollBottom = $(window).height() + $(this).scrollTop();
+
+  if($(this).scrollTop() >= $('#skill').position().top){
+    $('.done').addClass('animation');
+  }
+})

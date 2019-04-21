22242a2dc24a213fd206bc026c3b3f275eae0d3a
whkim94 <whkim94@gmail.com>
2019-04-21 03:23:36 -0700

    Modified some design and animation

--- script/script.js
+++ script/script.js
@@ -16,6 +16,7 @@ $(document).ready(function() {
 document.oncontextmenu = function () { return false; }
 document.onselectstart = function () { return false; }
 
+
 // Loads animation triggered once website is loaded
 
 $(window).on('load',function() {
@@ -23,13 +24,43 @@ $(window).on('load',function() {
 });
 
 
-// Progress animations is triggered when viewer scrolls down into skills section
+// Progress animations is triggered when viewer scrolls down into certain section
 
 $(document).on('scroll', function() {
-  // var scrollBottom = $(window).height() + $(this).scrollTop();
 
+  // Navbar background is triggered when scrolled down, and disappears when scrolls to top.
+  if($(this).scrollTop() > $('nav').position().top + 150){
+    $('nav').addClass('fixed');
+  } else {
+    $('nav').removeClass('fixed');
+  }
+
+  // Progress animation is triggered when scrolled down, and disappears when scrolls up.
   if($(this).scrollTop() >= $('#skill').position().top){
     $('.done').addClass('animation');
+  } else {
+    $('.done').removeClass('animation');
   }
 });
 
+
+
+
+// Circular progress animation for spoken language
+
+var count = $(('#count'));
+$({ Counter: 0 }).animate({ Counter: count.text() }, {
+  duration: 5000,
+  easing: 'linear',
+  step: function () {
+    count.text(Math.ceil(this.Counter)+ "%");
+  }
+});
+
+var s = Snap('#animated');
+var progress = s.select('#progress');
+
+progress.attr({strokeDasharray: '0, 251.2'});
+Snap.animate(0,251.2, function( value ) {
+    progress.attr({ 'stroke-dasharray':value+',251.2'});
+}, 5000);
